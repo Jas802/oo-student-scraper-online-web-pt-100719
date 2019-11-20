153e0b88db881a2d48eab8a1bc91b43ccd5a4918
Jas Spencer <jas.spencer414@gmail.com>
2019-11-20 13:37:18 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -32,7 +32,11 @@ class Scraper
         student[:blog] = link
       end
     end
+    student[:profile_quote] = profile_page.css(".profile_quote").text if profile_page.css(".profile_quote")
+    student[:bio] = profile_page.css("div.bio-content.content-holder div.description-holder p").text if profile_page.css("div.bio-content.content-holder div.description-holder p")
 
+    student
+  end
 
 
   end
